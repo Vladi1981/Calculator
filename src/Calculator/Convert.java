eff6550bc56ed2f77d9576c1d1921dc80b61c7b4
Администратор <davarashvilivlad@gmail.com>
2023-05-31 13:30:38 +0300

    Изменения логики калькулятора, дополнение комментариев

--- src/Calculator/Convert.java
+++ src/Calculator/Convert.java
@@ -1,27 +1,28 @@
 package Calculator;
 
-import java.util.Arrays;
+import java.util.Arrays; // импорт втроенной в java (jdk) библиотеки для работы с массивами Arrays
+import java.util.InputMismatchException;// импорт втроенной в java (jdk) библиотеки для работы с исключением InputMismatchException (несоответствие введеных данных запрашиваемым)
 
-import static Calculator.Main.firstNumber;
-import static Calculator.Main.secondNumber;
-import static Calculator.Main.firstRomanNumber;
-import static Calculator.Main.secondRomanNumber;
-import static Calculator.Main.operator;
+import static Calculator.Main.firstNumber; // импорт перемнной firstNumber из класса Main
+import static Calculator.Main.secondNumber; // импорт перемнной secondNumber из класса Main
+import static Calculator.Main.firstRomanNumber; // импорт перемнной firstRomanNumber из класса Main
+import static Calculator.Main.secondRomanNumber; // импорт перемнной secondRomanNumber из класса Main
+import static Calculator.Main.operator; // импорт перемнной operator из класса Main
 
-public class Convert {
-    public static void convert(String input) {
+public class Convert { // в этом методе , обрабатывается логика конвертации и парсинга (обработки) введенных данных из консоли
+    public static void convert(String input) { // объявляем метод convert (нижний регистр как в дргуих методах, то есть с маленькой буквы), который ничего не возвращает ( ключевое слово void), на вход ему подается переменнвя input типа String
 
-        char[] chars = input.toCharArray();
+        char[] chars = input.toCharArray();  // инициализируем массив символов chars, в него записываем результат выполнения метода toCharArray(встроен в jdk, находится в Arrays) над строковой переменной input, то есть этот метод преобразовывает в массив символов введенную в консоли строку
 
-        int charsLength = chars.length;
+        int charsLength = chars.length; // инициализируем переменную charsLength типа int, в которую складывается длинна строки chars (length - это метод вычисляет длинну строки он встроен в jdk)
 
-        String op = "+-*/";
-        String des = "IiVvXxLlCcDdMm";
+        String op = "+-*/"; // инициализируем строку op, в нее входят операторы
+        String des = "IVX";  // инициализируем строку des, в нее входят римские числа до 10
 
-        for (int i = 0; i < charsLength; i++) {
+        for (int i = 0; i < charsLength; i++) { // объявляем цикл for, он итерирует (пробегает циклически) переменную i столько раз сколько равно длинне строки charsLength со счетчиком i++ (означает что счетчик с каждой итерацией будет увеличиваться на 1)
 
-            if (op.indexOf(chars[i]) >= 0) {
-                operator = chars[i];
+            if (op.indexOf(chars[i]) >= 0) {  // условие логического ветвления if (если)
+                operator = chars[i];  // присваиваем переменной  operator итый индекс массива (или значение итого индекса массива)
 
                 char[] chr_firstNumber = Arrays.copyOfRange(chars, 0, i);
                 String str_firstNumber = String.valueOf(chr_firstNumber);
@@ -31,87 +32,75 @@ public class Convert {
                 String str_secondNumber = String.valueOf(chr_secondNumber);
                 str_secondNumber = str_secondNumber.trim();
 
-                if (des.indexOf(str_firstNumber.charAt(0)) >= 0) {
-                    firstNumber = convertToDecimal(str_firstNumber);
-                    firstRomanNumber = true;
+                if (des.indexOf(str_firstNumber.charAt(0)) >= 0) {  // условие логического ветвления if (если)
+                    firstNumber = romanToNumber(str_firstNumber);  // здесь уже в переменную firstNumber записывается результат выполнения метода romanToNumber (метод конвертации римских чиселв арабские)
+                    firstRomanNumber = true;  // переменной firstRomanNumber присваивается значение true (истина)
                 }
-                else {
-                    try {
-                        firstNumber = Integer.parseInt(str_firstNumber);
-                    }
-                    catch (NumberFormatException e) {
-                        firstNumber = -1;
+                else { // условие логического ветвления else (или)
+                    try { // открывается блок try
+                        firstNumber = Integer.parseInt(str_firstNumber); // в переменную firstNumber присваиваем результат выполнения метода parseInt (встроенный в jdk метод, точнее это метода класса обертки Integer), на вход методу подается переменная str_firstNumber типа String
+                    } // закрываем  блок try
+                    catch (NumberFormatException e) {  // ловим исключение
+                        firstNumber = -1; //  в переменную firstNumber присваиваем -1
                     }
                 }
 
-                if (des.indexOf(str_secondNumber.charAt(0)) >= 0) {
-                    secondNumber = convertToDecimal(str_secondNumber);
-                    secondRomanNumber = true;
+                if (des.indexOf(str_secondNumber.charAt(0)) >= 0) {  // условие логического ветвления if (если)
+                    secondNumber = romanToNumber(str_secondNumber);  // здесь уже в переменную secondNumber записывается результат выполнения метода romanToNumber (метод конвертации римских чиселв арабские)
+                    secondRomanNumber = true; // переменной secondRomanNumber присваивается значение true (истина)
                 }
-                else {
-                    try {
-                        secondNumber = Integer.parseInt(str_secondNumber);
+                else { // условие логического ветвления else (или)
+                    try { // открывается блок try
+                        secondNumber = Integer.parseInt(str_secondNumber); // в переменную secondNumber присваиваем результат выполнения метода parseInt (встроенный в jdk метод, точнее это метода класса обертки Integer), на вход методу подается переменная str_secondNumber типа String
                     }
-                    catch (NumberFormatException e) {
-                        secondNumber = -1;
+                    catch (NumberFormatException e) { // ловим исключение
+                        secondNumber = -1;  //  в переменную secondNumber присваиваем -1
                     }
                 }
             }
         }
 
     }
-
-    public static int convertToDecimal(String romanNumeral) {
-        int decimalNum = 0;
-
-        romanNumeral = romanNumeral.toUpperCase();
-
-        int l = romanNumeral.length();
-        int num = 0;
-        int previousnum = 0;
-        for (int i = l - 1; i >= 0; i--) {
-            char x = romanNumeral.charAt(i);
-            x = Character.toUpperCase(x);
-            switch (x) {
-                case 'I':
-                    previousnum = num;
-                    num = 1;
-                    break;
-                case 'V':
-                    previousnum = num;
-                    num = 5;
-                    break;
-                case 'X':
-                    previousnum = num;
-                    num = 10;
-                    break;
+    static String convertNumToRoman(int numArabian) { // объявляем метод convertNumToRoman он используется в классе Main, на вход ему подается переменная типа int
+        String[] roman = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+                "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+                "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+                "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+                "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+                "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+        };  // инициализируем массив стрингов roman
+        final String s = roman[numArabian];  // инициализируем финализированную переменную s типа String
+        return s;
+    }
+    private static int romanToNumber (String roman) { // объявляем метод romanToNumber, в нем переводятся римские в арабские
+        try { // открывается блок try
+            if (roman.equals("I")) { // условие логического ветвления if (если), римское число = I, возвращаемое значение =1
+                return 1;
+            } else if (roman.equals("II")) { // условие логического ветвления if (если), римское число = II, возвращаемое значение =2 и тд
+                return 2;
+            } else if (roman.equals("III")) {
+                return 3;
+            } else if (roman.equals("IV")) {
+                return 4;
+            } else if (roman.equals("V")) {
+                return 5;
+            } else if (roman.equals("VI")) {
+                return 6;
+            } else if (roman.equals("VII")) {
+                return 7;
+            } else if (roman.equals("VIII")) {
+                return 8;
+            } else if (roman.equals("IX")) {
+                return 9;
+            } else if (roman.equals("X")) {
+                return 10;
             }
-            if (num < previousnum) {
-                decimalNum = decimalNum - num;
-            }
-            else
-                decimalNum = decimalNum + num;
+        } catch (InputMismatchException e) { // ловим исключение
+            throw new InputMismatchException("Неверный формат данных"); // кидаем исключение с текстом "Неверный формат данных"
         }
-
-        return decimalNum;
+        return -1;
     }
 
-    public static String convertToRoman(int num) {
 
-        int[] values = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
-        String[] romanLiterals = {
-                "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
-        };
-
-        StringBuilder roman = new StringBuilder();
-
-        for (int i = 0; i < values.length; i++) {
-            while (num >= values[i]) {
-                num -= values[i];
-                roman.append(romanLiterals[i]);
-            }
-        }
-
-        return roman.toString();
-    }
 }
--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -1,73 +1,77 @@
-package Calculator;
-
-import java.util.Scanner;
-
-import static Calculator.Convert.convert;
-import static Calculator.Convert.convertToRoman;
+package Calculator; // пакет Calculator , где лежат классы Convert, Main
 
 
-public class Main {
-    static char operator; // Инициализация переменных с 11 - 14 строки
-    static int firstNumber;
-    static int secondNumber;
-    static boolean firstRomanNumber = false, secondRomanNumber = false;
+import java.util.InputMismatchException; // импорт встроенной библиотеки , проверяет на существование запрашиваемого элемента
+import java.util.Scanner;  // импорт встроенной библиотеки , сканер вводимых строк
 
-    // Точка входа в программу,выбрасывает исключения (в данном случае обработка ошибок)
+import static Calculator.Convert.*; // импорт всех методов (* - значит все) из класса Convert
+import static Calculator.Convert.convertNumToRoman;
 
-    public static void main(String args[]) throws Exception {
-        Scanner scan = new Scanner(System.in); // инициализируется объект типа сканер,объявляется с помощью слова new
-        System.out.print("Введите число1 математический оператор(+-/*) число2 ");
-        String input = scan.nextLine(); // Метод сканирует введенные строки
 
-        convert(input); // вызываем метод конвертер,на вход ему идет переменная инпат,результат сканирования строки
-        Integer result = calculate(); // записывается результат выполнения метода калькулятор
+public class Main { // объявляем класс Main - название токое же как файла Main.java
+    static char operator; // инициализируем переменную operator типа char (символьный тип)
+    static int firstNumber, secondNumber; // инициализируем переменные firstNumber , secondNumber типа int (целочисленный тип)
+    static boolean firstRomanNumber = false, secondRomanNumber = false; // инициализируем переменные firstRomanNumber (первое римское число), secondRomanNumber (второе римское число) типа boolean (примнимают два значения - false (ложь) или true (истина) )
 
-        // если результат выполнения не равен нулю,то выполняется следующая логика
-        if (result != null) {
-            if (firstRomanNumber == true & secondRomanNumber == true) {
-                System.out.println("Ответ: " + convertToRoman(result));
-            } else {
-                System.out.println("Ответ: " + result);
+    public static void main(String args[]) throws Exception { // объявляем метод main (нижний регистр или camelcase, то есть с маленькой буквы, как и все методы в Java), который в свою очередь кидает Исключение Exception
+        Scanner scan = new Scanner(System.in); // инициализируем объект scan типа Scanner, который будет считывать введенную в консоли строку
+        System.out.print("Введите число1 (0-10) потом математический оператор (+-/*) потом число2 (0-10): "); // выводим в консоль строку Введите число.....
+        String input = scan.nextLine();  // инициализируем переменную input, в которую попадает результат сканирования строки из консоли
+
+        convert(input); // вызываем метод convert, который ничего не возращает а просто выполняет логику конвертации римских чисел в арабские
+        Integer result = calculate(); // инициализируем переменную result, в которую попадает целочисленный результат выполнения метода calculate
+
+        if (result != null ) { // обявляем ветвление , если переменная result не равна null (ничто), то
+            if (firstRomanNumber == true & secondRomanNumber == true) { // если (первое римское число firstRomanNumber равно истина) и (второе римское число secondRomanNumber равно истина) то
+                System.out.println("Ответ: " + convertNumToRoman(result)); // в консоль выводится Ответ: результат выполнения метода convertToRoman
+            } else { // в другом случае (первое римское число firstRomanNumber равно ложь) и (второе римское число secondRomanNumber равно ложь), то есть это не римские числа)
+                System.out.println("Ответ: " + result);  // в консоль выводится Ответ: целочисленная переменная result
+            }
+        }
+        if (result <= 0) { // если переменная result меньеш или равна 0, то
+            if (firstRomanNumber == true & secondRomanNumber == true) { // если (первое римское число firstRomanNumber равно истина) и (второе римское число secondRomanNumber равно истина) то
+                throw new Exception("Результат 0 либо отрицательный результат вычитания римских чисел"); // кидаем исключение 'Отрицательный результат вычитания римских чисел либо результат 0'
+
             }
         }
     }
 
-    public static Integer calculate() throws Exception {
-        Integer result = null; // Инициализируется объект типа Int,целочисленного значения
+    public static Integer calculate() throws Exception { // объявляем метод calculate, он кидает исключение Exception (есть такой класс встроенный в java)
+        Integer result = null; // инициализируем переменную result типа Integer (целочисленный тип обертка над int), присваиваем ей значение null (ничто) - такие правила инициализации объектов в java
 
-        if (firstRomanNumber == secondRomanNumber) {
-            if (firstNumber < 0 || firstNumber > 10) {
-                throw new Exception("Первое число должно быть от 0-10 римскими,либо целочисленными");
+        if (firstRomanNumber == secondRomanNumber) { // условие - если firstRomanNumber (первое римское число) лигически равно (==) secondRomanNumber (второму римскому числу)
+            if ((firstNumber < 0 || firstNumber > 10) || (secondNumber < 0 || secondNumber > 10)) { // здесь логически выставляются условия ограничивающие первое и второе число ( < 0, > 10 )
+                throw new Exception("Первое или второе число должно быть от 0 до 10");  // кидаем или выбрасываем исключение через ключевое слово new (создает новый объект)
             }
-
-            if (secondNumber < 0 || secondNumber > 10) {
-                throw new Exception("Второе число должно быть от 0-10 римскими,либо целочисленными");
-            }
-
-            switch (operator) {
-                case '+':
-                    result = firstNumber + secondNumber;
+            switch (operator) {  // объявляем оператор switch case (логический оператор выбора, на основании входящих данных operator)
+                case '+': // случай когда operator + (сложение)
+                    result = firstNumber + secondNumber; // в переменную result записывается результат сложения firstNumber и secondNumber
                     break;
 
-                case '-':
+                case '-': // аналогично вычитание
                     result = firstNumber - secondNumber;
                     break;
 
-                case '*':
+                case '*': // аналогично умножение
                     result = firstNumber * secondNumber;
                     break;
 
-                case '/':
-                    result = firstNumber / secondNumber;
-                    break;
-
-                default: // случай по умолчанию
-                    throw new Exception("неправильный математический оператор");
+                case '/': // аналогично деление
+                    try { // здесь используется конструкция  try catch - логический блок, где выполняется операция деления, пока не кинется исключение ArithmeticException (арифметическое исключение - например джеление на 0) или  InputMismatchException (исключение когда введен неправильные данные)
+                        result = firstNumber / secondNumber; // в переменную result записывается результат деления firstNumber на secondNumber
+                    } catch (ArithmeticException | InputMismatchException e) { // блок где обрабатыываются исключения ArithmeticException и InputMismatchException, объект исключения записывается в переменную e
+                        System.out.println("Exception : " + e); // выводим в консоль строку с исключением
+                        System.out.println("Ошибка - деление на 0"); // выводим в консоль строку с текстом арифметического исключения
+                        break; // ключевое слово break - значит остановка выполнения программы, если кинулось исключение в результате операции деления
+                    }
+                    break; // ключевое слово break относится к общему случаю case '/'
+                default: // ключевое слово default означает, что по-умолчанию в конструкция switch case, кидается исключение в результате неверно введенного в консоли математического оператора, то есть вместо  (+, -, *, /) ввели например = или любой другой символ
+                    throw new IllegalArgumentException("Неверный знак операции, должно быть  (+, -, *, /)"); // само исключение IllegalArgumentException и что должно выводится в консоль, если оно произошло
             }
 
-            return result; // возврат значения из метода
-        } else {
-            throw new Exception("Оба числа должны быть римскими,либо целочисленными");
+            return result; // ключевое слово return относится к методу calculate, означает возврат значения result, в которую записывается результат вычислений метода
+        } else { // часть конструкции if else, означает что будет если не выполняются условия в 41 строке (смотрим вертикально от if на 72 строке стоит фигурная скобка)
+            throw new Exception("Оба числа должны быть римскими (IiVvXxLlCcDdMm) от 0 до 10"); // кидаем исключение и текст исключения
         }
     }
 }
