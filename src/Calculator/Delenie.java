30eda79c571deadb512515091c8dc82e79349b02
Администратор <davarashvilivlad@gmail.com>
2023-08-03 15:52:36 +0300

    Изменение файла деление

---
+++ src/Calculator/Delenie.java
@@ -0,0 +1,15 @@
+package Calculator;
+
+public class Delenie {
+
+    public static void main(String[] args) {
+        System.out.print(delenieab());
+    }
+    public static int delenieab(){
+        int a = 10;
+        int b = 2;
+
+        return a/b;
+
+    }
+}
---
+++ src/Calculator/Vladi.java
@@ -0,0 +1,23 @@
+package Calculator;
+
+public class Vladi {
+
+    public static void main(String[] args) {
+
+        step();
+    }
+
+    public static void step (){
+
+
+    int a = 5;
+    int b = 5;
+
+
+        System.out.println(a+b);
+        String input = "Я люблю жизнь";
+        System.out.println(input);
+    }
+
+
+}
