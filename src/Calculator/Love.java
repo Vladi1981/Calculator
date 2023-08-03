53eb92285b851dfc226d7222349cbff88e566874
Администратор <davarashvilivlad@gmail.com>
2023-08-03 15:48:49 +0300

    Изменения логики калькулятора, дополнение комментариев

---
+++ src/Calculator/Love.java
@@ -0,0 +1,17 @@
+package Calculator;
+
+public class Love {
+
+
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 6;
+
+        System.out.println(a+b);
+
+        String input =  "Я люблю сына";
+        System.out.println(input);
+
+    }
+}
---
+++ src/Calculator/Vladee.java
@@ -0,0 +1,9 @@
+package Calculator;
+
+public class Vladee {
+
+    public static void main(String[] args) {
+
+    }
+
+}
